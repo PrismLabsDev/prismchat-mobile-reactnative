4e1ddd563bcc8f49a4f5e9ca6119916228a75889
Jack Woodrow <jackwoodrow99@gmail.com>
2023-05-15 13:19:28 -0400

    Change logos and default theme to prism chat

--- App.tsx
+++ App.tsx
@@ -2,19 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>
+				Open up App.tsx to start working on your app!
+			</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		color: '#fff',
+		backgroundColor: '#262626',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		color: '#fff',
+	},
 });
